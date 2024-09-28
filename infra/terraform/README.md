ee6ede01e07d24aea006aa316e0bffd74ff4635f
stackgen-automation <automation@stackgen.com>
2024-09-28 09:48:56 +0000

    Automated commit

---
+++ infra/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with appCD.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/47cbd99a-8d80-47df-adea-edb6625ab265)
---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_lambda_hello_kitty_harsh_function_function_arn" {
+  value = module.appcd_477ae9b8-2fbb-5293-b835-eedd87db50b9.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_harsh_function_function_name" {
+  value = module.appcd_477ae9b8-2fbb-5293-b835-eedd87db50b9.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_harsh_function_function_url" {
+  value = module.appcd_477ae9b8-2fbb-5293-b835-eedd87db50b9.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_harsh_function_invoke_arn" {
+  value = module.appcd_477ae9b8-2fbb-5293-b835-eedd87db50b9.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_hello_kitty_harsh_role_arn" {
+  value = module.appcd_2fc706f9-6176-58c1-862d-78de66dfc1ea.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_hello_kitty_harsh_role_name" {
+  value = module.appcd_2fc706f9-6176-58c1-862d-78de66dfc1ea.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_harsh_function_arn" {
+  value = module.appcd_65b99755-8957-522f-9ed4-1242d9ecbad4.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_harsh_function_name" {
+  value = module.appcd_65b99755-8957-522f-9ed4-1242d9ecbad4.name
+  sensitive = false
+}
+
+
+output "aws_s3_harsh_arn" {
+  value = module.appcd_c6346a43-88b3-5c37-8fe8-a0c30e9b850b.arn
+  sensitive = false
+}
+
+
+output "aws_s3_harsh_bucket_name" {
+  value = module.appcd_c6346a43-88b3-5c37-8fe8-a0c30e9b850b.bucket_name
+  sensitive = false
+}
+
+
+output "aws_s3_harsh_bucket_website_endpoint" {
+  value = module.appcd_c6346a43-88b3-5c37-8fe8-a0c30e9b850b.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_harsh_kms_arn" {
+  value = module.appcd_c6346a43-88b3-5c37-8fe8-a0c30e9b850b.kms_arn
+  sensitive = false
+}
